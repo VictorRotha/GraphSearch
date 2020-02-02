c75dbc80aa699a0ba0355a218e6caa06d3e0a7f4
VictorRotha <Victor.Rotha@t-online.de>
2020-02-02 21:13:05 +0100

    DFS - DFS recursive - DFS iterative

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ d = Display(20,20)
 grid, start, target = d.make_grid()
 
 s = Search(grid, start, target)
-visited = s.BFS()
+# visited = s.DFS_iterative(branches=True)
+s.DFS_recursive(start)
+visited = s.visited
 path = s.make_path()
 
 d.draw_visited(visited)
--- search.py
+++ search.py
@@ -2,6 +2,7 @@ class Search:
     def __init__(self, grid, start, target):
         self.grid = grid
         self.start, self.target = start, target
+        self.visited = []
 
     def neighbours(self, pos):
         x, y = pos
@@ -19,7 +20,6 @@ class Search:
     def BFS(self):
         queue = [self.start]
         visited = [self.start]
-
         while queue:
             node = queue.pop(0)
             if node == self.target:
@@ -31,4 +31,45 @@ class Search:
                 queue.extend(nbs)
             visited.extend(nbs)
         return visited
+
+    def DFS_iterative(self, branches=False):
+        visited = [self.start]
+        stack = [self.start]
+        distance = 0
+        while stack:
+            node = stack.pop()
+            visited.append(node)
+            if node == self.target:
+                if branches:
+                    print('DFS TARGET FOUND AT', node, 'BRANCHES', self.grid[node][0], 'VISITED', len(visited))
+                else:
+                    print('DFS TARGET FOUND AT', node, 'DISTANCE', self.grid[node][0], 'VISITED', len(visited))
+                break
+
+            nbs = [nb for nb in self.neighbours(node) if nb not in visited]
+            stack.extend(nbs)
+
+            if branches:
+                if not nbs:
+                    distance += 1
+            else:
+                distance = self.grid[node][0] + 1
+
+            for nb in nbs:
+                self.grid[nb] = (distance, node)
+        return visited
+
+    def DFS_recursive(self, node):
+        self.visited.append(node)
+        if node == self.target:
+            print('DFS RECOURSIVE TARGET FOUND AT', node, 'DISTANCE', self.grid[node][0], 'VISITED', len(self.visited))
+            return True
+        nbs = self.neighbours(node)
+        distance = self.grid[node][0] + 1
+        for nb in nbs:
+            if nb not in self.visited:
+                self.grid[nb] = (distance, node)
+                if self.DFS_recursive(nb):
+                    return True
+        return False
 
